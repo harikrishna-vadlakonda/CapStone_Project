261e4826995502f7db01ab02a7de993be660ce9c
Harikrishna.Vadlakonda <harikrishna.vadlakonda@gmail.com>
2020-10-30 18:15:53 +0530

    Registration Page

---
+++ src/Components/Registration.js
@@ -0,0 +1,10 @@
+import React    from "react";
+import template from "./Registration.jsx";
+
+class Registration extends React.Component {
+  render() {
+    return template.call(this);
+  }
+}
+
+export default Registration;
