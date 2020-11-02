327bf7718de38647904fdf8bfb986f04a5688cdb
hari52618 <harikrishna.vadlakonda@gmail.com>
2020-11-03 00:04:26 +0530

    changes

--- src/Components/offers/offers.jsx
+++ src/Components/offers/offers.jsx
@@ -2,35 +2,73 @@ import React, { Component } from 'react';
 
 class Offers extends Component {
   render() {
-    return 
-    <div class="card-group">
-  <div class="card">
-    <img class="card-img-top" src="..." alt="Card image cap"/>
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    return <div className="card-comp">
+    <div className="container mt-3">
+      <div className="row">
+        <div className="col-sm-3">
+          <div className="card custCard h-20">
+            <div className="card-header p-0">
+            <img src={PlumberM} alt="plumber" width="100%" height="300px" />
+            </div>
+
+            <div className="card-body">
+               <h4>Plumber Services</h4>
+             <p class=" text-muted">Up to 50% off</p>
+               <button class="btn btn-primary btn-rounded btn-md">Explore</button>
+            </div>
+
+          </div>
+        </div>
+
+
+        <div className="col-sm-3">
+          <div className="card custCard">
+            <div className="card-header m-0 p-0">
+            <img src={Electrician} alt="plumber" width="100%" height="300px" />
+            </div>
+            <div className="card-body">
+               <h4>Electrician Services</h4>
+               <p class=" text-muted">Up to 30% off</p>
+               <button class="btn btn-primary btn-rounded btn-md">Explore</button>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="col-sm-3">
+          <div className="card custCard">
+            <div className="card-header m-0 p-0">
+            <img src={Carpenter} alt="plumber" width="100%" height="300px" />
+            </div>
+            <div className="card-body">
+               <h4>Carpenter Services</h4>
+               <p class=" text-muted">Up to 60% off</p>
+               <button class="btn btn-primary btn-rounded btn-md">Explore</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-sm-3">
+          <div className="card custCard">
+            <div className="card-header m-0 p-0">
+            <img src={BeautyS} alt="plumber" width="100%" height="300px" />
+            </div>
+            <div className="card-body">
+               <h4  class="card-title">Beauty Services</h4>
+               <p class=" text-muted">Up to 40% off</p>
+               <button class="btn btn-primary btn-rounded btn-md">Explore</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+      
     </div>
   </div>
-  <div class="card">
-    <img class="card-img-top" src="..." alt="Card image cap"/>
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="..." alt="Card image cap"/>
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-</div>
-  }
-  
-  }
+  };
+};
 
 export default Offers;
